96db0f640ad0f0fa3c98bd44fa4ec24867e7e4ce
kmit.akbar <akbarmd.mca@gmail.com>
2020-07-10 08:51:37 +0530

    first commit

---
+++ restapiwithboot/src/main/java/com/amex/loans/personal/RestapiwithbootApplication.java
@@ -0,0 +1,13 @@
+package com.amex.loans.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiwithbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiwithbootApplication.class, args);
+	}
+
+}
